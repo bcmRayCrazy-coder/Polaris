98260525e7fe2f7b4c6fe833bbbc748128a73576
bcm-ray_crazy <65066644+bcmRayCrazy-coder@users.noreply.github.com>
2023-01-03 10:20:23 +0800

    [feat]完成查看背包功能

--- src/game/commands/backpack/view.ts
+++ src/game/commands/backpack/view.ts
@@ -2,6 +2,10 @@ import { Command, CommandExecutor } from '../Command';
 import botConfig from '../../../bot.config';
 import { ItemTable } from '../../../db/types/ItemTable';
 import { getUserItems } from '../../../db/controller/ItemController';
+import { getPage, table2item } from '../../lib';
+import { Item } from '../../items/Item';
+import { client } from '../../bot';
+import { segment } from 'oicq';
 
 class ViewBackpackCommand extends Command {
     constructor() {
@@ -10,11 +14,33 @@ class ViewBackpackCommand extends Command {
     async execute(args: string[], executor: CommandExecutor): Promise<boolean> {
         if (args.length > 1) return false;
 
-        var items: ItemTable[] = await getUserItems(executor.id);
+        var items: Item[] = (await getUserItems(executor.id)).map((v) =>
+            table2item(v)
+        );
+        var page = 1;
         if (args.length == 1) {
-            var page = parseInt(args[0]);
+            page = parseInt(args[0]);
             if (isNaN(page)) return false;
+            if (Math.ceil(items.length / botConfig.itemsPerPage) <= page) {
+                // 不存在的页面
+                client.sendGroupMsg(executor.groupId, '查看失败: 页面不存在!');
+                return true;
+            }
         }
+        var pageItem = getPage(page, items);
+
+        var txt = segment.at(executor.id) + '的物品列表';
+        pageItem.forEach((v) => {
+            txt += '\n';
+            txt += `${v.name}`;
+        });
+        txt += '\n';
+        txt += `第 ${page}/${Math.ceil(
+            items.length / botConfig.itemsPerPage
+        )} 页, 翻页请用 #背包 查看 ${page + 1}`;
+
+        client.sendGroupMsg(executor.groupId, txt);
+
         return true;
     }
     help(): string {
